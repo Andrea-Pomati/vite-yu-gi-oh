44f0002a39359852397374b6cc8ce42b2f20d608
Andrea-Pomati <pomati.andrea@gmail.com>
2023-03-29 17:19:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import {store} from "./store.js";
+import axios from "axios";
+import AppMain from "./components/AppMain.vue";
+import AppLoader from "./components/AppLoader.vue";
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  components: {
+    AppMain,
+    AppLoader
+  },
+  created() {
+    
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=200&offset=0").then((response) => {
+      
+      console.log(response.data.data);
+      this.store.cards = response.data.data;
+      this.store.isLoading = false;
+    });
+  },
+}
+</script>
+
+<template>
+ 
+  <AppLoader v-if="store.isLoading"></AppLoader>
+
+  <div>
+
+    <AppMain></AppMain>
+
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
